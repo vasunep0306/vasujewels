9a9cfe25ebd3adb906c8a483fc516025913fbeb2
Vasanth Neppalli <vasunep0306@gmail.com>
2023-08-23 20:50:39 -0400

    new

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector2Int posIndex;
+    [HideInInspector]
+    public Board board;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetupGem(Vector2Int pos, Board theBoard)
+    {
+        posIndex = pos;
+        board = theBoard;
+    }
+}

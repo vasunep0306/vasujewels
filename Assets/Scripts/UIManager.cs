ccb7e71cebc12c2c0bf97192c70651642ac7b72b
Vasu Neppalli <vasunep0306@gmail.com>
2023-09-09 08:07:19 -0400

    Add timeText and scoreText variables to UIManager.cs and modify TestLevel.unity

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public TMP_Text timeText;
+    public TMP_Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
